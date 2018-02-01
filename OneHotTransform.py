78c14d2e82cf1fd4293203ac2706f8d8af1f8527
Rex Zhang <rexzhang2013@gmail.com>
2018-02-01 13:46:51 +0800

    Update OneHotTransform.py

--- OneHotTransform.py
+++ OneHotTransform.py
@@ -30,7 +30,7 @@ def OneHotTransform(df, ID=None, sparse=False) :
     df_data  = df.drop(ID,axis=1) if ID else df
     df_filled = df_data.fillna('NULL').apply(lambda x: x.astype(np.str),axis=0)
     onehot_names = OneHotName(df_filled)
-    df_factorized = df_data.apply(lambda x: x.astype('category',ordered=True).factorize()[0],axis=0)
+    df_factorized = df_filled.apply(lambda x: x.astype('category',ordered=True).factorize()[0],axis=0)
     enc = preprocessing.OneHotEncoder(categorical_features='all')
     enc.fit(df_factorized)
     onehot_data = enc.transform(df_factorized)
