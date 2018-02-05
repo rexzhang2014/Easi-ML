fec62a28f768dc6179736c8048dd837be6d4df72
Rex Zhang <rexzhang2013@gmail.com>
2018-02-05 18:14:11 +0800

    Update OneHotTransform.py
    Add logical parameter 'dropNaN'

--- OneHotTransform.py
+++ OneHotTransform.py
@@ -6,15 +6,16 @@ def OneHotName(df) :
     names = pd.Series()
     for col in df.columns :
         c = df[col]
-        names = names.append( pd.Series(col + '_' + np.unique(c)) )
-         
+        names = names.append( pd.Series(col + ':' + np.unique(c)) )
+    names.index = range(0,names.shape[0])     
     return names
 
-# %% OneHotTransform
+
 # Input: 
 #    df: a DataFrame to be transformed
 #    ID: the ID column if it is included in df; Default None
 #    sparse: logically determine return a sparse matrix or a DataFrame; Default False
+#    dropNaN: logic, convert NaN to 'NULL' if False, otherwise exclude it in result set
 # Rules: 
 # 0. assume all columns in df need be transformed and they will be changed to np.str type
 # 1. Fill NaN as string "NULL" and make it as one possible value.
@@ -25,7 +26,7 @@ def OneHotName(df) :
 # Return: 
 #   onehot_df: a DataFrame of one-hot array if 'sparse=False';
 #      otherwise a list of [sparse_matrix, columns]
-def OneHotTransform(df, ID=None, sparse=False) :
+def OneHotTransform(df, ID=None, sparse=False, dropNaN=False) :
     assert type(df) == pd.DataFrame
     df_data  = df.drop(ID,axis=1) if ID else df
     df_filled = df_data.fillna('NULL').apply(lambda x: x.astype(np.str),axis=0)
@@ -34,4 +35,9 @@ def OneHotTransform(df, ID=None, sparse=False) :
     enc = preprocessing.OneHotEncoder(categorical_features='all')
     enc.fit(df_factorized)
     onehot_data = enc.transform(df_factorized)
-    return [onehot_data, onehot_names] if sparse else pd.DataFrame(onehot_data.toarray(), columns=onehot_names.values)
+    if not sparse : 
+       onehot_data = pd.DataFrame(onehot_data.toarray(), columns=onehot_names.values)
+       if dropNaN :
+          onehot_names = onehot_names.drop(onehot_names.index[onehot_names.str.contains('NULL')])
+          onehot_data = onehot_data[onehot_names]
+    return [onehot_data, onehot_names] if sparse else onehot_data
